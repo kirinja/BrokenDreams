ff2e4387e422cc192dd7930e8bbc0a7cb28b50f7
LAPTOP-TB24N5LF\Malva <malva.forslin@gmail.com>
2017-03-22 13:57:29 +0100

    no message

---
+++ Assets/Sounds/Scripts/SurfaceCollisionType.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurfaceCollisionType : MonoBehaviour {
+
+	public enum Mode {Default, Deadzone}
+	public Mode terrainType;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public string GetTerrainType(){
+
+		string typeString = "";
+
+		switch (terrainType) {
+
+		case Mode.Default:
+			typeString = "Default";
+			break;
+		case Mode.Deadzone:
+			typeString = "Deadzone";
+			break;
+		default:
+			typeString = "";
+			break;
+		}
+
+		return typeString;
+
+	}
+}
---
+++ Assets/Sounds/Scripts/TriggerSound.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerSound : MonoBehaviour {
+
+	private AudioSource[] sources;
+	private AudioSource source1;
+
+	public AudioClip wallSound;	//when colliding with a wall 
+	public AudioClip playerSound;
+
+	// Use this for initialization
+	void Start () {
+		sources = GetComponents<AudioSource> ();
+		source1 = sources[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision col){
+		Debug.Log ("Collision");
+		if (col.gameObject.CompareTag("Wall")){
+			Debug.Log ("Wall");
+			source1.PlayOneShot (wallSound);
+
+		}
+	}
+}
