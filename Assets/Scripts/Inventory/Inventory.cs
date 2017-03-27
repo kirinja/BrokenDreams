57203410450e1590546fa93334602715ea56c802
LAPTOP-FKQ2FND9\Marika <marika.bendes@gmail.com>
2017-03-27 14:38:39 +0200

    GUI är KLAR

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -6,6 +6,8 @@ public class Inventory : MonoBehaviour {
 
 	public int MaxSlots = 4;
 
+	private int activeSlots;
+
 	public InventorySlotUI[] slots; //HÄR SKA ABILITY KLASSEN VARA
     public Controller3D controller3D;
 
@@ -19,13 +21,25 @@ public class Inventory : MonoBehaviour {
 	}
 
 	void Start () {
-		slots[0].Selected();
-        //GameObject.Find("Player").GetComponent<Controller3D>().SetAbility(0);
-        controller3D.SetAbility(0);
+		var playerAttribute = controller3D.GetComponent<PlayerAttributes> ();
+
+		for (var i = 0; i < playerAttribute.Abilities.Count; i++) {
+			slots [i].gameObject.SetActive (true);
+			activeSlots = i + 1;
+		}
+			
+		if (activeSlots > 0) {
+			slots[0].Selected();
+			//GameObject.Find("Player").GetComponent<Controller3D>().SetAbility(0);
+			controller3D.SetAbility(0);
+		}
+			
+
+
     }
 
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+		if (Input.GetKeyDown (KeyCode.Alpha1) && activeSlots > 0) {
 			slots[0].Selected ();
 			slots [1].Unselected ();
 			slots [2].Unselected ();
@@ -34,7 +48,7 @@ public class Inventory : MonoBehaviour {
 		    //GameObject.Find("Player").GetComponent<Controller3D>().SetAbility(0);
             controller3D.SetAbility(0);
 
-		} else if (Input.GetKeyDown (KeyCode.Alpha2)) {
+		} else if (Input.GetKeyDown (KeyCode.Alpha2) && activeSlots > 1) {
 			slots[0].Unselected ();
 			slots [1].Selected ();
 			slots [2].Unselected ();
@@ -44,7 +58,7 @@ public class Inventory : MonoBehaviour {
             controller3D.SetAbility(1);
 
         }
-        else if (Input.GetKeyDown (KeyCode.Alpha3)) {
+		else if (Input.GetKeyDown (KeyCode.Alpha3) && activeSlots > 2) {
 			slots[0].Unselected ();
 			slots [1].Unselected ();
 			slots [2].Selected ();
@@ -54,7 +68,7 @@ public class Inventory : MonoBehaviour {
             controller3D.SetAbility(2);
 
         }
-        else if (Input.GetKeyDown (KeyCode.Alpha4)) {
+		else if (Input.GetKeyDown (KeyCode.Alpha4) && activeSlots > 3) {
 			slots[0].Unselected ();
 			slots [1].Unselected ();
 			slots [2].Unselected ();
@@ -65,6 +79,17 @@ public class Inventory : MonoBehaviour {
 
         }
     }
+
+	public void addAbility(){
+		if (activeSlots < MaxSlots) {
+			if(activeSlots == 0){
+				slots [activeSlots].Selected ();
+				controller3D.SetAbility(0);
+			}
+			slots [activeSlots++].gameObject.SetActive (true);
+		}
+
+	}
 
 //	public InventorySlot GetSlot (int slotIndex) {
 	//	if(slotIndex < 0 || slotIndex >= slotCount) {
