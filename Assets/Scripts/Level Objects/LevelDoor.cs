5a3c5130c768444fc3e70936736fa29816b3fe73
Anton Södergren <anton.sodergren@gmail.com>
2017-03-27 12:27:43 +0200

    Updated GameManager to use names

--- Assets/Scripts/Level Objects/LevelDoor.cs
+++ Assets/Scripts/Level Objects/LevelDoor.cs
@@ -9,6 +9,14 @@ public class LevelDoor : MonoBehaviour
     public string Scene;
     public bool RequiresInput = true;
 
+    private void Start()
+    {
+        if (!GameObject.FindObjectOfType<GameManager>().LevelAvailable(Scene))
+        {
+            enabled = false;
+        }
+    }
+
     private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player") && (!RequiresInput || Input.GetAxisRaw("Vertical") >= 0.5f))
