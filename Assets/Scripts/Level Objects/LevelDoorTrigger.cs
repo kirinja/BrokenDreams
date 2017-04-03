f53cd95f8a0d38baa77769e3f7df39a08d8f884a
Niklas Blom Laptop <blom.niklas@gmail.com>
2017-04-03 15:01:07 +0200

    Fixed GameManager (and saving/loading), Added level transitions

--- Assets/Scripts/Level Objects/LevelDoorTrigger.cs
+++ Assets/Scripts/Level Objects/LevelDoorTrigger.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Prime31.TransitionKit;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -13,7 +14,26 @@ public class LevelDoorTrigger : MonoBehaviour
     {
         if (other.CompareTag("Player") && (!RequiresInput || Input.GetAxisRaw("Vertical") >= 0.5f))
         {
-            SceneManager.LoadScene(Scene);
+            //var fishEye = new FishEyeTransition()
+            //{
+            //    nextScene = Scene,
+            //    duration = 2.0f,
+            //    size = 0.2f,
+            //    zoom = 100.0f,
+            //    colorSeparation = 0.1f
+            //};
+            //TransitionKit.instance.transitionWithDelegate(fishEye);
+
+            var doorway = new DoorwayTransition()
+            {
+                nextScene = Scene,
+                duration = 1.0f,
+                perspective = 1.5f,
+                depth = 3f,
+                runEffectInReverse = true
+            };
+            TransitionKit.instance.transitionWithDelegate(doorway);
+            //SceneManager.LoadScene(Scene);
         }
     }
 }
