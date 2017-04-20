121a0f33485daac4dacb4ca82790374f0ad87269
Deku Scrub <marika.bendes@gmail.com>
2017-04-20 14:50:14 +0200

    Nu är HUD:en uppdaterad med nya hjärtan!

---
+++ Assets/Scripts/Heart.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Heart : MonoBehaviour {
+
+	private RawImage image;
+
+
+	void Start () {
+		image = GetComponent<RawImage> ();
+	}
+	
+
+	void Update () {
+		
+	}
+
+	public void SetFilled(bool isFilled){
+		if (isFilled) {
+			image.color = new Color (image.color.r, image.color.g, image.color.b, 1f);
+
+
+		} else {
+			
+			image.color = new Color (image.color.r, image.color.g, image.color.b, 0.1f);
+
+		}
+ 
+
+	}
+		
+}
