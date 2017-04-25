fbaa42e0ce0762969fe2093468228bbf037d4fb9
Anton Södergren <anton.sodergren@gmail.com>
2017-04-25 11:49:54 +0200

    Respawning now reloads level from checkpoint instead of just resetting player.

---
+++ Assets/CheckPointHandler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+
+public class CheckPointHandler : MonoBehaviour
+{
+    // Use this for initialization
+    private void Start()
+    {
+        var gm = GameManager.Get();
+        if (gm)
+            if (gm.UseCheckPoint)
+                gm.LoadCheckPointData();
+
+        var player = GameObject.FindGameObjectWithTag("Player");
+        if (!player) return;
+
+        var controller = player.GetComponent<Controller3D>();
+        if (controller)
+            controller.Spawn();
+    }
+}
