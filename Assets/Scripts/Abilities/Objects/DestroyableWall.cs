2e85f1c85fe53512091b32cc7b801ade4a4b1d12
Anton Södergren <anton.sodergren@gmail.com>
2017-04-26 16:27:45 +0200

    DESTROYABLE WALLS CAN EXPLODE!

--- Assets/Scripts/Abilities/Objects/DestroyableWall.cs
+++ Assets/Scripts/Abilities/Objects/DestroyableWall.cs
@@ -1,17 +1,57 @@
 ﻿using UnityEngine;
 
-//[RequireComponent(typeof(Renderer))]
 [RequireComponent(typeof(Collider))]
 [RequireComponent(typeof(AudioSource))]
 public class DestroyableWall : Attackable
 {
+    public float FadeTime = 0.2f;
+
+    private bool _destroyed;
+    private Timer _fadeTimer;
+
+
+    private void Start()
+    {
+        _destroyed = false;
+        _fadeTimer = new Timer(FadeTime);
+    }
+
+
     public override void Damage(int damage = 1)
     {
-        //GetComponent<Renderer>().enabled = false;
+        if (_destroyed) return;
+
+        _destroyed = true;
+        
         GetComponent<Collider>().enabled = false;
+
+        var rigidBodies = GetComponentsInChildren<Rigidbody>();
+        foreach (var body in rigidBodies)
+        {
+            body.isKinematic = false;
+        }
 
         var source = GetComponent<AudioSource>();
         source.Play();
-        Destroy(this.gameObject, source.clip.length);
+    }
+
+
+    private void Update()
+    {
+        if (!_destroyed) return;
+
+        if (_fadeTimer.Update(Time.deltaTime))
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            var renderers = GetComponentsInChildren<Renderer>();
+            foreach (var childRenderer in renderers)
+            {
+                childRenderer.material.color = new Color(childRenderer.material.color.r, childRenderer.material.color.g,
+                    childRenderer.material.color.b, 1 - _fadeTimer.PercentDone);
+            }
+        }
     }
 }
