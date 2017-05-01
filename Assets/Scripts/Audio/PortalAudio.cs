8923c17285ba63ff2633adc0b6fa2f01dc75761f
Niklas Blom <blom.niklas@gmail.com>
2017-05-01 11:12:18 +0200

    Moved AudioListener from camera to Player (this way we can use 3D spatial audio in an easier way). Added sounds to portals in Hub (low humming)

---
+++ Assets/Scripts/Audio/PortalAudio.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof (AudioSource))]
+public class PortalAudio : MonoBehaviour
+{
+    public AudioClip Clip;
+    public bool Loop = true;
+    private AudioSource _source;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _source = GetComponent<AudioSource>();
+	    _source.clip = Clip;
+	    _source.loop = Loop;
+	    _source.time = Random.Range(0, Clip.length);
+        _source.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
