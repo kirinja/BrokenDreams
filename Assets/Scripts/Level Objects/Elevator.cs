beebe5bc7d56029ea36484c568a3d05bae5ce0cd
Niklas Blom <blom.niklas@gmail.com>
2017-05-10 13:33:04 +0200

    Fixat så hissen i level 2 funkar

--- Assets/Scripts/Level Objects/Elevator.cs
+++ Assets/Scripts/Level Objects/Elevator.cs
@@ -5,32 +5,17 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class Elevator : MonoBehaviour
 {
-    public float ElevatorWaitTime = 4.0f;
-    public GameObject[] enemies;
+    public float ElevatorWaitTime = 2.0f;
+    public GameObject[] buttons;
 
     private bool active;
-    public GameObject Feedback1;
-    public GameObject Feedback2;
-    public GameObject Feedback3;
-    public GameObject Feedback4;
-    private GameObject[] FeedbackList;
 
     // Use this for initialization
     void Start ()
 	{
 	    active = false;
 	    GetComponent<Animator>().enabled = false;
-        FeedbackList = new GameObject[4];
-	    FeedbackList[0] = Feedback1;
-	    FeedbackList[1] = Feedback2;
-	    FeedbackList[2] = Feedback3;
-	    FeedbackList[3] = Feedback4;
-
-	    for (int i = 0; i < FeedbackList.Length; i++)
-	    {
-	        FeedbackColor(i, Color.blue);
-	    }
-
+        
     }
 	
 	// Update is called once per frame
@@ -40,18 +25,10 @@ public class Elevator : MonoBehaviour
         var activate = true;
 
         // HACK pretty badly done but it will do for now
-	    for (int i = 0; i < enemies.Length; ++i)
+	    for (int i = 0; i < buttons.Length; ++i)
 	    {
-	        if (enemies[i] && enemies[i].GetComponent<Enemy>().Alive)
-	        {
+	        if (buttons[i] && !buttons[i].GetComponent<ElevatorButton>().Active)
 	            activate = false;
-                FeedbackColor(i, Color.blue);
-	            //break;
-	        }
-	        else
-	        {
-	            FeedbackColor(i, Color.green);
-	        }
 	    }
 
 	    if (activate)
@@ -68,90 +45,5 @@ public class Elevator : MonoBehaviour
         yield return new WaitForSeconds(ElevatorWaitTime);
         GetComponent<Animator>().enabled = true;
     }
-
-    private void FeedbackColor(int i, Color color)
-    {
-        foreach (MeshRenderer r in FeedbackList[i].GetComponentsInChildren<MeshRenderer>())
-        {
-            r.sharedMaterial.color = color;
-        }
-    }
-
-    void OnEnable()
-    {
-        if (Feedback1)
-        {
-            foreach (MeshRenderer r in Feedback1.GetComponentsInChildren<MeshRenderer>())
-            {
-                r.sharedMaterial.color = Color.blue;
-            }
-        }
-
-        if (Feedback2)
-        {
-            foreach (MeshRenderer r in Feedback2.GetComponentsInChildren<MeshRenderer>())
-            {
-                r.sharedMaterial.color = Color.blue;
-            }
-        }
-
-        if (Feedback3)
-        {
-            foreach (MeshRenderer r in Feedback3.GetComponentsInChildren<MeshRenderer>())
-            {
-                r.sharedMaterial.color = Color.blue;
-            }
-        }
-
-        if (Feedback4)
-        {
-            foreach (MeshRenderer r in Feedback4.GetComponentsInChildren<MeshRenderer>())
-            {
-                r.sharedMaterial.color = Color.blue;
-            }
-        }
-    }
-
-    void OnDisable()
-    {
-        if (Feedback1)
-        {
-            foreach (MeshRenderer r in Feedback1.GetComponentsInChildren<MeshRenderer>())
-            {
-                r.sharedMaterial.color = Color.blue;
-            }
-        }
-
-        if (Feedback2)
-        {
-            foreach (MeshRenderer r in Feedback2.GetComponentsInChildren<MeshRenderer>())
-            {
-                r.sharedMaterial.color = Color.blue;
-            }
-        }
-
-        if (Feedback3)
-        {
-            foreach (MeshRenderer r in Feedback3.GetComponentsInChildren<MeshRenderer>())
-            {
-                r.sharedMaterial.color = Color.blue;
-            }
-        }
-
-        if (Feedback4)
-        {
-            foreach (MeshRenderer r in Feedback4.GetComponentsInChildren<MeshRenderer>())
-            {
-                r.sharedMaterial.color = Color.blue;
-            }
-        }
-    }
-
-    void OnApplicationQuit()
-    {
-        for (int i = 0; i < FeedbackList.Length; i++)
-        {
-            FeedbackColor(i, Color.blue);
-        }
-    }
+    
 }
---
+++ Assets/Scripts/Level Objects/ElevatorButton.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorButton : MonoBehaviour {
+
+    public bool Active;
+    public GameObject Feedback;
+    public GameObject OtherButton;
+
+    // Use this for initialization
+    void Start () {
+        if (!Feedback)
+            throw new ArgumentNullException("Feedback cant be null, please assign a feedback");
+        foreach (MeshRenderer r in Feedback.GetComponentsInChildren<MeshRenderer>())
+        {
+            r.sharedMaterial.color = Color.blue;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerExit(Collider other)
+    {
+        if (!OtherButton.GetComponent<ElevatorButton>().Active)
+        {
+            if (other.CompareTag("Player") || other.CompareTag("Movable Object"))
+            {
+                Active = false;
+
+                foreach (MeshRenderer r in Feedback.GetComponentsInChildren<MeshRenderer>())
+                {
+                    r.sharedMaterial.color = Color.blue;
+                }
+            }
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        Debug.Log("test");
+        if (other.CompareTag("Player") || other.CompareTag("Movable Object"))
+        {
+            Active = true;
+
+            foreach (MeshRenderer r in Feedback.GetComponentsInChildren<MeshRenderer>())
+            {
+                r.sharedMaterial.color = Color.green;
+            }
+        }
+    }
+
+    void OnEnable()
+    {
+        foreach (MeshRenderer r in Feedback.GetComponentsInChildren<MeshRenderer>())
+        {
+            r.sharedMaterial.color = Color.blue;
+        }
+    }
+
+    void OnDisable()
+    {
+        if (!Feedback) return;
+        foreach (MeshRenderer r in Feedback.GetComponentsInChildren<MeshRenderer>())
+        {
+            r.sharedMaterial.color = Color.blue;
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        foreach (MeshRenderer r in Feedback.GetComponentsInChildren<MeshRenderer>())
+        {
+            r.sharedMaterial.color = Color.blue;
+        }
+    }
+}
