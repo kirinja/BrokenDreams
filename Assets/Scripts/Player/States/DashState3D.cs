21f8148fc3e23300b9d50b45080da443d2873281
Anton Södergren <anton.sodergren@gmail.com>
2017-05-12 13:19:07 +0200

    Dash particles work and stuff

--- Assets/Scripts/Player/States/DashState3D.cs
+++ Assets/Scripts/Player/States/DashState3D.cs
@@ -35,9 +35,7 @@ internal class DashState3D : ICharacterState3D
     public void Enter()
     {
         _dashTimer.Reset();
-
-        _controller.transform.Find("Dash").Find("DashTrail").GetComponent<ParticleSystem>().Play();
-        _controller.transform.Find("Dash").Find("Sparkle").GetComponent<ParticleSystem>().Play();
+        _controller.transform.Find("Dash").GetComponentInChildren<ParticleSystem>().Play();
     }
 
 
@@ -55,8 +53,7 @@ internal class DashState3D : ICharacterState3D
             ? new Vector2(_previousVelocity.x, 0f)
             : new Vector2(Mathf.Sign(_previousVelocity.x) * _controller.GetComponent<PlayerAttributes>().MaxSpeed, 0f);
 
-        _controller.transform.Find("Dash").Find("DashTrail").GetComponent<ParticleSystem>().Stop();
-        _controller.transform.Find("Dash").Find("Sparkle").GetComponent<ParticleSystem>().Stop();
+        _controller.transform.Find("Dash").GetComponentInChildren<ParticleSystem>().Stop();
     }
 
 
